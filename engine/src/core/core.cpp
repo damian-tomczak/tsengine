12a2165b1b4afed700e5d8d519d7765729193075
Wit111 <dominoxo111@gmail.com>
2023-10-31 13:17:13 +0100

    cyberith movement - conversion from wchar_t

--- engine/src/core/core.cpp
+++ engine/src/core/core.cpp
@@ -33,93 +33,93 @@ __forceinline void runCleaner()
 // TODO: maybe would be possible to fancy break down run function?
 namespace ts
 {
-    [[deprecated("not implemented yet")]]
-    unsigned getTickCount()
+[[deprecated("not implemented yet")]]
+unsigned getTickCount()
+{
+    return tickCount;
+}
+
+int run(Engine* const engine) try
+{
+    std::lock_guard<std::mutex> _{ engineInit };
+
+    if (!engine)
     {
-        return tickCount;
+        LOGGER_ERR("Game pointer is invalid");
     }
 
-    int run(Engine* const engine) try
+    if (isAlreadyInitiated)
     {
-        std::lock_guard<std::mutex> _{ engineInit };
+        LOGGER_ERR("Game is already initiated");
+    }
+    isAlreadyInitiated = true;
 
-        if (!engine)
-        {
-            LOGGER_ERR("Game pointer is invalid");
-        }
+    const auto testerAdapter = dynamic_cast<TesterEngine*>(engine);
 
-        if (isAlreadyInitiated)
-        {
-            LOGGER_ERR("Game is already initiated");
-        }
-        isAlreadyInitiated = true;
+    unsigned width{ 1280u }, height{ 720u };
+    engine->init(width, height);
 
-        const auto testerAdapter = dynamic_cast<TesterEngine*>(engine);
+    if (!std::filesystem::is_directory("assets"))
+    {
+        LOGGER_ERR("Assets can not be found");
+    }
 
-        unsigned width{ 1280u }, height{ 720u };
-        engine->init(width, height);
+    compileShaders("assets/shaders");
 
-        if (!std::filesystem::is_directory("assets"))
-        {
-            LOGGER_ERR("Assets can not be found");
-        }
+    Context ctx;
+    ctx.createOpenXrContext().createVulkanContext();
 
-        compileShaders("assets/shaders");
+    auto window = Window::createWindowInstance(GAME_NAME, width, height);
+    MirrorView mirrorView{ ctx, window };
+    mirrorView.createSurface();
+    ctx.createVkDevice(mirrorView.getSurface());
+    Headset headset{ ctx };
+    headset.init();
+    Controllers controllers(ctx.getXrInstance(), headset.getXrSession());
+    controllers.setupControllers();
 
-        Context ctx;
-        ctx.createOpenXrContext().createVulkanContext();
+    std::shared_ptr<Model> ruins = std::make_shared<Model>(Model{
+        .model = math::Mat4(1.f),
+        .pipeline = PipelineType::NORMAL_LIGHTING,
+        });
+    std::shared_ptr<Model> polonez = std::make_shared<Model>(Model{
+        .pos = {0.f, 0.f, -10.f},
+        .model = math::Mat4(1.f),
+        .pipeline = PipelineType::NORMAL_LIGHTING,
+        });
+    std::shared_ptr<Model> sphere1 = std::make_shared<Model>(Model{
+        .pos = {0.f, 1.5f, -5.f},
+        .model = math::Mat4(1.f),
+        .pipeline = PipelineType::PBR,
+        .material = Materials::create(Material::Type::GOLD),
+        });
 
-        auto window = Window::createWindowInstance(GAME_NAME, width, height);
-        MirrorView mirrorView{ ctx, window };
-        mirrorView.createSurface();
-        ctx.createVkDevice(mirrorView.getSurface());
-        Headset headset{ ctx };
-        headset.init();
-        Controllers controllers(ctx.getXrInstance(), headset.getXrSession());
-        controllers.setupControllers();
+    const std::vector<std::shared_ptr<Model>> models{
+        ruins,
+        polonez,
+        sphere1,
+    };
 
-        std::shared_ptr<Model> ruins = std::make_shared<Model>(Model{
-            .model = math::Mat4(1.f),
-            .pipeline = PipelineType::NORMAL_LIGHTING,
-            });
-        std::shared_ptr<Model> polonez = std::make_shared<Model>(Model{
-            .pos = {0.f, 0.f, -10.f},
-            .model = math::Mat4(1.f),
-            .pipeline = PipelineType::NORMAL_LIGHTING,
-            });
-        std::shared_ptr<Model> sphere1 = std::make_shared<Model>(Model{
-            .pos = {0.f, 1.5f, -5.f},
-            .model = math::Mat4(1.f),
-            .pipeline = PipelineType::PBR,
-            .material = Materials::create(Material::Type::GOLD),
-            });
+    auto meshData = std::make_unique<MeshData>();
+    meshData->loadModel("assets/models/village.obj", models, 1);
+    meshData->loadModel("assets/models/polonez.obj", models, 1);
+    meshData->loadModel("assets/models/sphere.obj", models, 1);
 
-        const std::vector<std::shared_ptr<Model>> models{
-            ruins,
-            polonez,
-            sphere1,
-        };
+    Renderer renderer{ ctx, headset, models, std::move(meshData) };
+    renderer.createRenderer();
+    mirrorView.connect(&headset, &renderer);
 
-        auto meshData = std::make_unique<MeshData>();
-        meshData->loadModel("assets/models/village.obj", models, 1);
-        meshData->loadModel("assets/models/polonez.obj", models, 1);
-        meshData->loadModel("assets/models/sphere.obj", models, 1);
+    LOGGER_LOG("tsengine initialization completed successfully");
 
-        Renderer renderer{ ctx, headset, models, std::move(meshData) };
-        renderer.createRenderer();
-        mirrorView.connect(&headset, &renderer);
+    window->show();
+    bool isRenderingStarted{};
+    math::Vec3 cameraPosition{};
+    auto loop = true;
+    auto previousTime = std::chrono::high_resolution_clock::now();
+    auto startTime = std::chrono::steady_clock::now();
+    // TODO: display message to wear the headset
+    // TODO: consider if we should provide an option to render firstly to the window then copy it to the headset
 
-        LOGGER_LOG("tsengine initialization completed successfully");
-
-        window->show();
-        bool isRenderingStarted{};
-        math::Vec3 cameraPosition{};
-        auto loop = true;
-        auto previousTime = std::chrono::high_resolution_clock::now();
-        auto startTime = std::chrono::steady_clock::now();
-        // TODO: display message to wear the headset
-        // TODO: consider if we should provide an option to render firstly to the window then copy it to the headset
-        
 #ifdef CYBSDK_FOUND
         CybSDK::VirtDevice* device = CybSDK::Virt::FindDevice();
         if (device == nullptr)
@@ -128,58 +128,71 @@ namespace ts
         }
 
         const CybSDK::VirtDeviceInfo& info = device->GetDeviceInfo();
-        const wchar_t* ptr = L"Virtualizer Elite 2";
-        LOGGER_LOG(("Device found "s + char(*info.ProductName) + "Firmware Version: "s + char(info.MajorVersion) + "."s + char(info.MinorVersion)).c_str());
+        const wchar_t* virtualizerName = info.ProductName;
+        size_t virtualizerNameLen = 0;
+        if (wcstombs_s(&virtualizerNameLen, nullptr, 0, virtualizerName, 0) == 0)
+        {
+            char* mbstr = new char[virtualizerNameLen];
+            wcstombs_s(&virtualizerNameLen, mbstr, virtualizerNameLen, virtualizerName, virtualizerNameLen);
+            std::string virtualizerConvertedName(mbstr);
+            delete[] mbstr;
+            LOGGER_LOG(("Device found "s + virtualizerConvertedName + "Firmware Version: " + std::to_string(info.MajorVersion) + "." + std::to_string(info.MinorVersion)).c_str());
+        }
+        else
+        {
+            LOGGER_WARN("Failed to convert virtualizer Product Name");
+        }
+        
 
         if (!device->Open())
         {
             LOGGER_ERR("Unable to connect to Cyberith Virtualizer");
         }
 #endif
-       
-        while (loop)
+
+    while (loop)
+    {
+        // I have no idea how to better implement it.
+        if ((testerAdapter != nullptr) && isRenderingStarted)
         {
-            // I have no idea how to better implement it.
-            if ((testerAdapter != nullptr) && isRenderingStarted)
-            {
-                if (std::chrono::steady_clock::now() >= (startTime + testerAdapter->renderingDuration))
-                {
-                    loop = false;
-                }
-            }
-
-            if (headset.isExitRequested())
+            if (std::chrono::steady_clock::now() >= (startTime + testerAdapter->renderingDuration))
             {
                 loop = false;
             }
+        }
 
-            auto message = window->peekMessage();
-            if (message == Window::Message::QUIT)
+        if (headset.isExitRequested())
+        {
+            loop = false;
+        }
+
+        auto message = window->peekMessage();
+        if (message == Window::Message::QUIT)
+        {
+            loop = false;
+        }
+        else if (message == Window::Message::RESIZE)
+        {
+            mirrorView.onWindowResize();
+        }
+        window->dispatchMessage();
+
+        const auto nowTime = std::chrono::high_resolution_clock::now();
+        const long long elapsedNanoseconds =
+            std::chrono::duration_cast<std::chrono::nanoseconds>(nowTime - previousTime).count();
+        constexpr auto nanosecondsPerSecond = 1e9f;
+        const auto deltaTime = static_cast<float>(elapsedNanoseconds) / nanosecondsPerSecond;
+        previousTime = nowTime;
+
+        uint32_t swapchainImageIndex;
+        const auto frameResult = headset.beginFrame(swapchainImageIndex);
+
+        if (frameResult == Headset::BeginFrameResult::RENDER_FULLY)
+        {
+            if (!isRenderingStarted)
             {
-                loop = false;
+                isRenderingStarted = true;
             }
-            else if (message == Window::Message::RESIZE)
-            {
-                mirrorView.onWindowResize();
-            }
-            window->dispatchMessage();
-
-            const auto nowTime = std::chrono::high_resolution_clock::now();
-            const long long elapsedNanoseconds =
-                std::chrono::duration_cast<std::chrono::nanoseconds>(nowTime - previousTime).count();
-            constexpr auto nanosecondsPerSecond = 1e9f;
-            const auto deltaTime = static_cast<float>(elapsedNanoseconds) / nanosecondsPerSecond;
-            previousTime = nowTime;
-
-            uint32_t swapchainImageIndex;
-            const auto frameResult = headset.beginFrame(swapchainImageIndex);
-
-            if (frameResult == Headset::BeginFrameResult::RENDER_FULLY)
-            {
-                if (!isRenderingStarted)
-                {
-                    isRenderingStarted = true;
-                }
 
 #ifdef CYBSDK_FOUND
                 float ring_height = device->GetPlayerHeight();
@@ -224,29 +237,24 @@ namespace ts
                 }
 #endif
 
-                
+            renderer.render(cameraPosition, swapchainImageIndex);
+            const auto mirrorResult = mirrorView.render(swapchainImageIndex);
 
+            const auto isMirrorViewVisible = (mirrorResult == MirrorView::RenderResult::VISIBLE);
+            renderer.submit(isMirrorViewVisible);
 
-
-
-                renderer.render(cameraPosition, swapchainImageIndex);
-                const auto mirrorResult = mirrorView.render(swapchainImageIndex);
-
-                const auto isMirrorViewVisible = (mirrorResult == MirrorView::RenderResult::VISIBLE);
-                renderer.submit(isMirrorViewVisible);
-
-                if (isMirrorViewVisible)
-                {
-                    mirrorView.present();
-                }
-            }
-
-            if ((frameResult == Headset::BeginFrameResult::RENDER_FULLY) ||
-                (frameResult == Headset::BeginFrameResult::RENDER_SKIP_PARTIALLY))
+            if (isMirrorViewVisible)
             {
-                headset.endFrame(frameResult == Headset::BeginFrameResult::RENDER_SKIP_PARTIALLY);
+                mirrorView.present();
             }
         }
+
+        if ((frameResult == Headset::BeginFrameResult::RENDER_FULLY) ||
+            (frameResult == Headset::BeginFrameResult::RENDER_SKIP_PARTIALLY))
+        {
+            headset.endFrame(frameResult == Headset::BeginFrameResult::RENDER_SKIP_PARTIALLY);
+        }
+    }
 
     engine->close();
     ctx.sync();
